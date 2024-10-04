64aee75d8715241881115098913e294c57df1406
Ali Mousazada <mousazada.ali@gmail.com>
2024-10-04 19:21:53 -0400

    create router

---
+++ routes/router.js
@@ -0,0 +1,21 @@
+
+const express = require('express')
+const router =  express.Router()
+const Appointment = require('../models/appintmentModel')
+const path = require('path')
+
+router.post('/schedule_service' , async(req , res) => {
+    const {name , email , phone , date} = req.body
+
+    try {
+        const newAppointment = new Appointment({name , email , phone , date})
+        await newAppointment.save()
+        res.sendFile(path.join(__dirname , '../views/bookResult.html'))
+    } catch(err) {
+
+        console.log(err);
+        res.sendFile(path.join(__dirname , '../views/bookError.html'))
+    }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -20,4 +20,5 @@ mongoose.connect(process.env.DB_CONNECTION)
 .catch(err => console.log(err))
 
 const PORT = process.env.PORT || 3001
+
 app.listen(PORT , () => console.log(`Server running on port ${PORT}`))
